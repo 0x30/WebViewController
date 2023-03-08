7cc0c214246afd5398ca0d14158769bbebab4d5a
jingwenzheng <alphabetion@gmail.com>
2023-03-08 16:38:00 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebViewController
+
+A description of this package.
---
+++ Sources/WebViewController/WebViewController.swift
@@ -0,0 +1,6 @@
+public struct WebViewController {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
